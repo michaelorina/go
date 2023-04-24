96ac4c702cc943d0afd7e8fe029d9a9243e733f1
michaelorina <orinamichael4@gmail.com>
2023-04-24 21:41:25 +0300

    logical operators

--- operators.go
+++ operators.go
@@ -21,4 +21,8 @@ func main() {
   var x = 5
   var y = 3
   fmt.Println(x>y) // returns 1 (true) because 5 is greater than 3
+
+//   x < 5 &&  x < 10
+//   x < 5 || x < 4
+//   !(x < 5 && x < 10)
 }
