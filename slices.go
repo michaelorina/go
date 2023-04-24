aba7c6292ff04448b6d552c5296c6bb1fabdb8fb
michaelorina <orinamichael4@gmail.com>
2023-04-24 21:34:38 +0300

    go slices

---
+++ slices.go
@@ -0,0 +1,28 @@
+package main
+import ("fmt")
+
+func main() {
+  myslice1 := []int{}
+  fmt.Println(len(myslice1))
+  fmt.Println(cap(myslice1))
+  fmt.Println(myslice1)
+
+  myslice2 := []string{"Go", "Slices", "Are", "Powerful"}
+  fmt.Println(len(myslice2))
+  fmt.Println(cap(myslice2))
+  fmt.Println(myslice2)
+
+  var myarray = [length]datatype{values} // An array
+  myslice := myarray[start:end] // A slice made from the array
+
+  myslice3 := make([]int, 5, 10)
+  fmt.Printf("myslice1 = %v\n", myslice3)
+  fmt.Printf("length = %d\n", len(myslice3))
+  fmt.Printf("capacity = %d\n", cap(myslice3))
+
+  // with omitted capacity
+  myslice4 := make([]int, 5)
+  fmt.Printf("myslice2 = %v\n", myslice4)
+  fmt.Printf("length = %d\n", len(myslice4))
+  fmt.Printf("capacity = %d\n", cap(myslice4))
+}
